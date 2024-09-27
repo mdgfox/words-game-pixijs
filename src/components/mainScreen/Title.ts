8a110c435c7c825d28a438ee910b1a57bb101e2f
Alex Khmelev <sashaaka4774@gmail.com>
2024-09-28 02:45:25 +0300

    Fix resize scale calc to min w or h

--- src/components/mainScreen/Title.ts
+++ src/components/mainScreen/Title.ts
@@ -19,7 +19,7 @@ export class Title extends Container {
     }
 
     handleResize() {
-        const scale = window.innerHeight / Game.HEIGHT;
+        const scale = Math.min(window.innerWidth / Game.WIDTH, window.innerHeight / Game.HEIGHT);
         this.position.set(window.innerWidth / scale / 2, 50);
     }
 
